7b60af5bde40d7c941c7fbfaa6a82bae177e164b
IvanMladenov <ismladenov@gmail.com>
2015-05-20 14:37:44 +0300

    commit first problem

---
+++ HomeworkFilesAndStreams/FilesAndStreams/P1_OddLines/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace P1_OddLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileToRead = @"E:\Fundamentials\FilesAndStreams\HomeworksProjects\text.txt";
+            StreamReader read = new StreamReader(fileToRead);
+            int rowCounter = 0;
+            while(!read.EndOfStream)
+            {
+                string readLine = read.ReadLine();
+                if(rowCounter%2==0)
+                {
+                    Console.WriteLine(readLine);
+                }
+                rowCounter++;
+            }
+        }
+    }
+}
